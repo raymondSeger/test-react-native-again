79d8957ab88387cac8403c28b8cc1f795103a4fc
Raymond Seger <raymondseger@live.com>
2018-11-25 14:19:44 +0700

    styles likes CSS for <Text>

--- App.js
+++ App.js
@@ -3,7 +3,19 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { Greeting } from './Greeting.js';
 import { Blink } from './Blink.js';
 
+const styles = StyleSheet.create({
+    bigblue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    red: {
+        color: 'red',
+    },
+});
+
 export default class App extends React.Component {
+
     render() {
         let pic = {
             uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
@@ -12,9 +24,8 @@ export default class App extends React.Component {
         return (
             <View style={styles.container}>
                 <Blink text='I love to blink' />
-                <Blink text='Yes blinking is so great' />
-                <Blink text='Why did they ever take this out of HTML' />
-                <Blink text='Look at me look at me look at me' />
+
+                <Text style={styles.red}>just red</Text>
 
                 <Greeting name='Rexxar' />
                 <Greeting name='Jaina' />
@@ -25,12 +36,3 @@ export default class App extends React.Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex            : 1,
-        backgroundColor : '#fff',
-        alignItems      : 'center',
-        justifyContent  : 'center',
-    },
-});
