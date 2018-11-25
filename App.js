5425d7c9394b22f2e54ed7a00b6be6eb0767bab6
Raymond Seger <raymondseger@live.com>
2018-11-25 14:28:45 +0700

    use Platform for android/ios and Touchables for tapping gestures
    use Platform for android/ios and Touchables for tapping gestures https://facebook.github.io/react-native/docs/handling-touches

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Button, StyleSheet, Text, View, Image } from 'react-native';
+import { Alert, Platform, Button, StyleSheet, Text, View, Image, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native';
 import { Greeting } from './Greeting.js';
 import { Blink } from './Blink.js';
 import {PizzaTranslator} from "./PizzaTranslator";
@@ -17,12 +17,30 @@ const styles = StyleSheet.create({
     red: {
         color: 'red',
     },
+    container: {
+        paddingTop: 60,
+        alignItems: 'center'
+    },
+    button: {
+        marginBottom: 30,
+        width: 260,
+        alignItems: 'center',
+        backgroundColor: '#2196F3'
+    },
+    buttonText: {
+        padding: 20,
+        color: 'white'
+    }
 });
 
 export default class App extends React.Component {
 
     _onPressButton() {
         Alert.alert('You tapped the button!')
+    }
+
+    _onLongPressButton() {
+        Alert.alert('You long-pressed the button!')
     }
 
     render() {
@@ -41,6 +59,44 @@ export default class App extends React.Component {
                 </View>
 
                 <PizzaTranslator />
+
+                <View style={styles.container}>
+
+                    <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>TouchableHighlight</Text>
+                        </View>
+                    </TouchableHighlight>
+
+                    <TouchableOpacity onPress={this._onPressButton}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>TouchableOpacity</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableNativeFeedback
+                        onPress={this._onPressButton}
+                        background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+                        </View>
+                    </TouchableNativeFeedback>
+
+                    <TouchableWithoutFeedback
+                        onPress={this._onPressButton}
+                    >
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+                        </View>
+                    </TouchableWithoutFeedback>
+
+                    <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Touchable with Long Press</Text>
+                        </View>
+                    </TouchableHighlight>
+
+                </View>
 
                 <Blink text='I love to blink' />
 
